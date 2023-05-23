0fc219c1cc440ef8f9c805c25f1debb6c9b7b76a
Jose Secchi <jhsecchi@gmail.com>
2023-05-23 14:14:30 -0300

    creo componente navBar

---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav__brand">
+                    <a href="#" className="nav__link">Meus Petatis</a>
+                </div>
+                <ul className="nav__list">
+                    <li>
+                        <a href="#" className="nav__link">Categories</a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav__link">Tiendas</a>
+                    </li>
+                    <li>
+                        <a href="#" className="nav__link">Nosotros</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
